17d9c863187fe9b0b67b034635859189a9f5bd07
Usuario <Usuario@CPD02-M09>
2022-08-25 15:44:54 -0300

    Adicionar arquivos de projeto.

---
+++ hotel/hotel/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.Generic;
+
+using hotel.model;
+using hotel.view;
+
+namespace hotel
+{
+    public partial class App : Application
+    {
+
+        public List<DadosUsuario> list_usuario = new List<DadosUsuario>
+        {
+            new DadosUsuario()
+            {
+                Email = "apaulinineto@gmail.com",
+                Nome = "Adib", 
+                Senha = "123"
+
+            }
+
+        };
+        public App()
+        {
+            InitializeComponent();
+            if (Properties.ContainsKey("usuario_logado"))
+                MainPage = new Protegida();
+            else
+            MainPage = new Login();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ hotel/hotel/model/DadosUsuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hotel.model
+{
+    public class DadosUsuario
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+    }
+}
